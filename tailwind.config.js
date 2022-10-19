e3e06e888910d42ea1c20b32c901bb32722f8ee8
Xandowski <xandy-moraes@hotmail.com>
2022-10-18 22:45:15 -0300

    criado layout inicial

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      theme:{
+        
+      },
+      fontFamily: {
+        sans: ["Roboto", "sans-serif"],
+      }
+    },
+  },
+  plugins: [],
+}
