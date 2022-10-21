2fd2aa398dcc4f388f0d737a954f5f2e942a4c49
Xandowski <xandy-moraes@hotmail.com>
2022-10-21 01:09:47 -0300

    fix classes not loading tailwindcss

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,37 +1,35 @@
-// import { getPokemons } from './api.js'
+import { pokeAPI } from './api.js'
 
-let offset = 10
-let limit = 10
-let url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+let offset = 0
+
 const pokemonList = document.querySelector('[data-js="pokemon-list"]')
 const next = document.querySelector('[data-js="next"]')
 const prev = document.querySelector('[data-js="prev"]')
 
 next.addEventListener('click', () => {
+  offset+=10
   removePokemonsItems()
-  offset +=10
-  url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-  getPokemons()
+  offset >= 10 ? prev.classList.remove('hidden') : prev.classList.add('hidden')
+  loadPokemonItens(offset)
 }) 
 
 prev.addEventListener('click', () => {
+  offset-=10
   removePokemonsItems()
-  offset -=10
-  url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
-  getPokemons()
+  loadPokemonItens(offset)
 })
 
-function createPokemonItem(details) {  
-  const id = details.id < 10 ? `00${details.id}` : details.id < 100 ? `0${details.id}` : details.id
-  
-  return `<li class="bg-[#159F6E] flex flex-col rounded-2xl p-3 max-w-[224px]">
-    <span class="text-green-800 font-semibold self-end">${id}</span>
-    <h3 class="flex justify-between font-semibold">${details.name}</h3>
+function createPokemonItem(pokemon) {
+  const id = pokemon.id < 10 ? `00${pokemon.id}` : pokemon.id < 100 ? `0${pokemon.id}` : pokemon.id
+
+  return `<li class="bg-${pokemon.types[0].type.name}-500 flex flex-col rounded-2xl p-3 max-w-[224px]">
+    <span class="text-${pokemon.types[0].type.name}-800 font-semibold self-end capitalize">${id}</span>
+    <h3 class="flex justify-between font-semibold">${pokemon.name}</h3>
     <div class="grid grid-cols-2">
       <div class="flex flex-col justify-center">
-        ${details.types.map(type => `<span class="bg-[#60e1ca] px-2 py-1 w-fit rounded-2xl mb-1 text-zinc-100 font-bold">${type.type.name}</span>`).join('')}
+        ${pokemon.types.map(type => `<span class="bg-${type.type.name}-300 px-2 py-1 w-fit rounded-2xl mb-1 text-zinc-100 font-bold">${type.type.name}</span>`).join('')}
       </div>
-      <img class="w-28 h-28 self-end" src="${details.sprites.other.dream_world.front_default}" alt="${details.name}">
+      <img class="w-28 h-28 self-end" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
     </div>
   </li>`
 }
@@ -40,20 +38,28 @@ function removePokemonsItems(){
   pokemonList.innerHTML = ''
 }
 
-async function getPokemons(){
-  const pokemons = await fetch(url)
-    .then(res => res.json())
-    .then(data => data)
-
-  offset === 0 ? prev.classList.add('invisible') : prev.classList.remove('invisible')
-
-  pokemons.results.forEach(async pokemon => {
-    const details = await fetch(pokemon.url)
-      .then(res => res.json())
-      .then(data => data)
-  
-    pokemonList.innerHTML += createPokemonItem(details)
+function loadPokemonItens(offset) {
+  offset > 10 ? prev.classList.remove('hidden') : prev.classList.add('hidden')
+  pokeAPI.getPokemons(offset).then((pokemons = []) => {
+    const html = pokemons.map(createPokemonItem).join('')
+    pokemonList.innerHTML += html
+    
   })
 }
 
-getPokemons()
+loadPokemonItens(offset)
+
+function createPokemonDetails(pokemon) {
+  const id = pokemon.id < 10 ? `00${pokemon.id}` : pokemon.id < 100 ? `0${pokemon.id}` : pokemon.id
+
+  return `<div class="flex flex-col items-center">
+    <img class="w-28 h-28" src="${pokemon.sprites.other.dream_world.front_default}" alt="${pokemon.name}">
+    <h3 class="font-semibold">${pokemon.name}</h3>
+    <span class="text-${pokemon.types[0].type.name}-800 font-semibold">${id}</span>
+    <div class="grid grid-cols-2">
+      <div class="flex flex-col justify-center">
+        ${pokemon.types.map(type => `<span class="bg-${pokemon.types[0].type.name}-300 px-2 py-1 w-fit rounded-2xl mb-1 text-zinc-100 font-bold">${type.type.name}</span>`).join('')}
+      </div>
+    </div>
+  </div>`
+}
